f81d927e18b44d7c5da6e7f48460d54d447da807
Kleber Figueredo <kleberf.dev@gmail.com>
2023-07-10 01:13:46 -0300

    feat: entrada de dados

---
+++ main.py
@@ -0,0 +1,11 @@
+combustivel = ['Gasolina', 5.90, 'Etanol', 4.89, 'Diesel', 7.89]
+login = []
+senha = []
+pagamento = ['Débito', 'Crédito', 'Pix', 'Dinheiro']
+
+#Entrada de Dados
+print('Qual o Combustível?\n1 - Gasolina\n2 - Etanol\n3 - Diesel')
+opcombustivel = input('Opção: ')
+print('Qual a forma de pagamento?\n1 - Débito\n2 - Crédito\n3 - Pix\n4 - Dinheiro')
+oppagamento = input('Opção: ')
+qtdlitros = float(input('Qual a quantidade de litros?\nLitros: '))
